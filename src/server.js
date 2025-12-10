1727167c7f68a182639b2045c74840cc8dfac2f6
LIUDMILA KIK <tender.ird@gmail.com>
2025-12-10 10:47:04 +0100

    branch 2

--- src/server.js
+++ src/server.js
@@ -1,78 +1,27 @@
-// src/server.js
+import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
-import pino from 'pino-http';
-import 'dotenv/config';
+import { connectMongoDB } from './db/connectMongoDB.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import logger from './middleware/logger.js'
+import notesRouter from './routes/notesRoutes.js'
+import {errorHandler} from './middleware/errorHandler.js'
 
 
-const app = express();
-const PORT = process.env.PORT ?? 3000;
+export const app = express();
+export const PORT = process.env.PORT || 3000;
 
-// Middleware
-app.use(express.json()); // Дозволяє обробляти дані у форматі JSON, які надходять у body запиту.
-app.use(cors()); // Дозволяє запити з будь-яких джерел
-app.use(
-  pino({
-    level: 'info',
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        colorize: true,
-        translateTime: 'HH:MM:ss',
-        ignore: 'pid,hostname',
-        messageFormat:
-          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
-        hideObject: true,
-      },
-    },
-  }),
-);
+app.use(logger)
+app.use(express.json())
+app.use(cors())
 
-// Логування часу
-app.use((req, res, next) => {
-  console.log(`Time: ${new Date().toLocaleString()}`);
-  next();
-});
+app.use(notesRouter)
 
-// Перший маршрут
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello world!' });
-});
+app.use(notFoundHandler);
+app.use(errorHandler)
 
-// GET-запит до маршруту "/notes"
-app.get('/notes', (req, res) => {
-  res.status(200).json({
-    message: 'Retrieved all notes',
-  });
-});
+await connectMongoDB()
 
-// Конкретний користувач за id
-app.get('/notes/:noteId', (req, res) => {
-  const { noteId } = req.params;
-  res.status(200).json({
-    message: `Retrieved note with ID: ${noteId}`,
-  });
-});
-
-// Маршрут для тестування middleware помилки
-app.get('/test-error', () => {
-  // Штучна помилка для прикладу
-  throw new Error('Simulated server error');
-});
-
-// Middleware 404 (після всіх маршрутів)
-app.use((req, res) => {
-  res.status(404).json({ message: 'Route not found' });
-});
-
-// Middleware для обробки помилок
-app.use((err, req, res, next) => {
-  res.status(500).json({
-    message: err.message,
-  });
-});
-
-// Запуск сервера
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
